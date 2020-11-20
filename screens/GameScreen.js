aa3f1a2ffb581ec41a5665153878201ddbe7d555
theo82 <theoaristi53@gmail.com>
2020-11-20 07:29:30 +0200

    ScrollView & Flexbox (Yes, that works)

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -26,21 +26,19 @@ const generateRandomBetween = (min, max, exclude) => {
   )
 
   const GameScreen = props => {
-
     const initialGuess = generateRandomBetween(1, 100, props.userChoice);
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
     const [pastGuesses, setPastGuesses] = useState([initialGuess]);
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
-
+  
     const { userChoice, onGameOver } = props;
-
   
     useEffect(() => {
-        if(currentGuess === userChoice) {
-            props.onGameOver(pastGuesses.length);
-        }
-    },[currentGuess, userChoice, onGameOver])
+      if (currentGuess === userChoice) {
+        onGameOver(pastGuesses.length);
+      }
+    }, [currentGuess, userChoice, onGameOver]);
   
     const nextGuessHandler = direction => {
       if (
@@ -63,57 +61,63 @@ const generateRandomBetween = (min, max, exclude) => {
         currentGuess
       );
       setCurrentGuess(nextNumber);
-      setPastGuesses(curPastGuesses => [nextNumber, ...curPastGuesses])
+      // setRounds(curRounds => curRounds + 1);
+      setPastGuesses(curPastGuesses => [nextNumber, ...curPastGuesses]);
     };
-
-  return (
-    <View style={styles.screen}>
-      <Text style={DefaultStyles.title}>Opponent's Guess</Text>
-      <NumberContainer>{currentGuess}</NumberContainer>
-      <Card style={styles.buttonContainer}>
-        <MainButton onPress={nextGuessHandler.bind(this, 'lower')} >
-          <Ionicons name="md-remove" size={24} color="white" />
-        </MainButton> 
-        <MainButton onPress={nextGuessHandler.bind(this, 'greater')} >
-          <Ionicons name="md-add" size={24} color="white" />
-        </MainButton>
-      </Card>
-      <View style={styles.list}>
-        <ScrollView>
+  
+    return (
+      <View style={styles.screen}>
+        <Text style={DefaultStyles.title}>Opponent's Guess</Text>
+        <NumberContainer>{currentGuess}</NumberContainer>
+        <Card style={styles.buttonContainer}>
+          <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
+            <Ionicons name="md-remove" size={24} color="white" />
+          </MainButton>
+          <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>
+            <Ionicons name="md-add" size={24} color="white" />
+          </MainButton>
+        </Card>
+        <View style={styles.listContainer}>
+          <ScrollView contentContainerStyle={styles.list}>
             {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
-        </ScrollView>
+          </ScrollView>
+        </View>
       </View>
-      
-    </View>
-  );
-  }
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    padding: 10,
-    alignItems: 'center'
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginTop: 20,
-    width: 300,
-    maxWidth: '80%',
-    maxWidth: "90%"
-  },
-  list: {
-    flex: 1,
-    width: '80%'
-  },
-  listItem: {
-    borderColor: '#ccc',
-    borderWidth: 1,
-    padding: 15,
-    marginVertical: 10,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
-});
+    );
+  };
+  const styles = StyleSheet.create({
+    screen: {
+      flex: 1,
+      padding: 10,
+      alignItems: 'center'
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      marginTop: 20,
+      width: 400,
+      maxWidth: '90%'
+    },
+    listContainer: {
+      flex: 1,
+      width: '80%'
+    },
+    list: {
+      flexGrow: 1,
+      alignItems: 'center',
+      justifyContent: 'flex-end'
+    },
+    listItem: {
+      borderColor: '#ccc',
+      borderWidth: 1,
+      padding: 15,
+      marginVertical: 10,
+      backgroundColor: 'white',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '60%'
+    }
+  });
+  
 
 export default GameScreen;
