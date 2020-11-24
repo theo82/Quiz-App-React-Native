bbfbac825c8de489f6510549653df5b657bc4ef1
theo82 <theoaristi53@gmail.com>
2020-11-24 14:34:49 +0200

    Listening to Orientation Changes

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -24,6 +24,7 @@ const StartGameScreen = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [confirmed, setConfirmed] = useState(false);
   const [selectedNumber, setSelectedNumber] = useState();
+  const [buttonWidth, setButtonWidth ] = useState(Dimensions.get('window').width / 4);
 
   const numberInputHandler = inputText => {
     setEnteredValue(inputText.replace(/[^0-9]/g, ''));
@@ -33,6 +34,17 @@ const StartGameScreen = props => {
     setEnteredValue('');
     setConfirmed(false);
   };
+
+  useEffect(() => {
+    const updateLayout = () => {
+      setButtonWidth(Dimensions.get('window').width / 4);
+    };
+  
+    Dimensions.addEventListener('change', updateLayout);
+    return () => {
+      Dimensions.removeEventListener('change', updateLayout);
+    }
+  })
 
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
@@ -90,14 +102,14 @@ const StartGameScreen = props => {
                 value={enteredValue}
               />
               <View style={styles.buttonContainer}>
-                <View style={styles.button}>
+                <View style={{width: buttonWidth}}>
                   <Button
                     title="Reset"
                     onPress={resetInputHandler}
                     color={Colors.accent}
                   />
                 </View>
-                <View style={styles.button}>
+                <View style={{width: buttonWidth}}>
                   <Button
                     title="Confirm"
                     onPress={confirmInputHandler}
@@ -137,9 +149,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingHorizontal: 15
   },
-  button: {
-    width: Dimensions.get('window').width/4
-  },
+  // button: {
+  //   width: Dimensions.get('window').width/4
+  // },
   input: {
     width: 50,
     textAlign: 'center'
