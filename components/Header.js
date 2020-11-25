b80511be81ef2a6ef74cb335174aaa3dffb20d74
theo82 <theoaristi53@gmail.com>
2020-11-25 18:54:02 +0200

    Using Platform-specific Code Files

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Platform } from 'react-native';
 
 import TitleText from './TitleText';
 import Colors from '../constants/colors';
 
 const Header = props => {
     return (
-        <View style={styles.header}>
+        <View 
+            style={{
+                ...styles.headerBase,
+                ...Platform.select({
+                    ios: styles.headerIOS,
+                    android: styles.headerAndroid
+                })
+            }}
+        >
             <TitleText style={styles.title}>{props.title}</TitleText>
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
       width: '100%',
       height: 90,
       paddingTop: 36,
@@ -22,6 +30,14 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
       borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
+    },
+    headerIOS: {
+        backgroundColor: 'white',
+        borderBottomColor: '#ccc',
+        borderBottomWidth:  1
+    },
+    headerAndroid: {
+        backgroundColor: Colors.primary
     },
     title: {
       color: Platform.OS === 'ios' ? Colors.primary : 'white'
